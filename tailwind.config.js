b1a3e531df138eed0568b26e52f43b584f4baefe
amirhossein allami <captain2480722023@gmail.com>
2025-02-27 22:51:08 +0330

    Bug Fixing ( Tech Stack cards color )

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
+  safelist: [
+    'bg-green-100', 'text-green-800', 'dark:bg-green-900', 'dark:text-green-300',
+    'bg-blue-100', 'text-blue-800', 'dark:bg-blue-900', 'dark:text-blue-300',
+    'bg-yellow-100', 'text-yellow-800', 'dark:bg-yellow-900', 'dark:text-yellow-300'
+  ],
   theme: {
     extend: {
       boxShadow: {
